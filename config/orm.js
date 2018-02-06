a0225d8b8e16b9aece6c5b9114256e5e18fdd0ac
Mac User <alexarobinson19@gmail.com>
2018-02-05 21:42:42 -0500

    finished the orm.js file with the inquiries to sql

--- config/orm.js
+++ config/orm.js
@@ -2,14 +2,33 @@
 var connection = require('./connection.js');
 //set up three functions called:
 var orm = {
+//selectAll() function that will pull all of the results in the database
+selectAll:function(tableInput, cb){
+    var queryString=`SELECT * FROM ${tableInput} ASC BY id`;
+    connection.query(queryString, function(error, data){
+        if(error) throw error;
+        cb(data);
+    });
+},
+//insertOne()
+insertOne:function(tableInput, col, val, cb){
+    var queryString = `INSERT INTO ${tableInput}(${col})VALUES(${val})`;
+    connection.query(queryString, vals, function(error, data){
+        if(error) throw error;
+        cb(data);
+    });
+},
+//updateOne() function that will update the Boolean value
+updateOne:function(col, val, id_col, id, cb){
+    var queryString = `SET {${col}:${val}} WHERE ${id_col}=${id}`;
+    connection.query(queryString, function(error, data){
+        if(error) throw error;
+        cb(data);
+    });
+}
+
 
 }
-//selectAll()
-//insertOne()
-//updateOne()
 
-//these run SQL commands to retrieve and store data
-
-//export this in module.exports
-
+//exported in module.exports
 module.exports=orm;
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,7 @@
 //import the orm with require
-
+var orm = require('../config/orm.js');
 //call the ORM functions "using burger specific input for the ORM."
-
+var buger = {
+    
+}
 //export at the end of the file
