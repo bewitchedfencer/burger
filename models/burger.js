084a469530b191381e846c62af8ce88be92811ee
Mac User <alexarobinson19@gmail.com>
2018-02-05 21:53:30 -0500

    completed the burger.js in the models folder, calling each of the three functions in the orm.js

--- models/burger.js
+++ models/burger.js
@@ -2,6 +2,21 @@
 var orm = require('../config/orm.js');
 //call the ORM functions "using burger specific input for the ORM."
 var buger = {
-    
+    selectBurgers:function(cb){
+        orm.selectAll('burger', function(result){
+            cb(result);
+        });
+    },
+    createBurger:function(burgerName, cb){
+        orm.insertOne('burger', 'burger_name', burgerName, function(result){
+            cb(result);
+        });
+    },
+    eatBurger:function(id, cb){
+        orm.updateOne('devoured', false, 'id', id, function(result){
+            cb(result);
+        });
+    }
 }
 //export at the end of the file
+module.exports=burger;
